a947d4a8d44f54db1b1c65fdd67459d4dcbfd65f
Gerardo Morales <geramrico@Gerardos-MacBook-Pro.local>
2023-02-04 11:02:01 -0600

    adding all

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+
+// Top level component that includes every component
+// Optional
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps}/>
+}
+
